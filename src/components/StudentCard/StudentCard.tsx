77ed2c11e7dde50e2f399b1ef16c3e5cb9e2231b
Mario Pardo <mariio.pardojr@gmail.com>
2022-02-28 22:56:14 -0300

    feat: create StudentCard

---
+++ src/components/StudentCard/StudentCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Student } from '../../services/types';
+import AvatarIcon from '../../assets/user-purple.svg'
+import fonts from '../../styles/fonts';
+
+interface StudentCardProps {
+  student: Student
+}
+
+const StudentCard: React.FC<StudentCardProps> = ({ student, ...rest }) => {
+  console.log(student)
+  return (
+    <TouchableOpacity style={style.container} {...rest}>
+      {student.imageUrl ? <Image style={style.avatar} source={{ uri: student.imageUrl }} /> : <AvatarIcon width={55} height={55}/>}
+      <Text style={style.studentName}>{student.name}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const style = StyleSheet.create({
+  container: {
+    width: 160,
+    height: 160,
+    backgroundColor: '#FA743E',
+    borderRadius: 20,
+    margin: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  studentName: {
+    fontFamily: fonts.heading,
+    fontSize: 16,
+    color: '#fff',
+    margin: 10
+  }
+})
+
+export default StudentCard;
