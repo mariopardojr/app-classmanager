63cc9350e5ab18c1412acd61704d0311a3a21db3
Mario Pardo <mariio.pardojr@gmail.com>
2022-03-05 06:13:08 -0300

    fix: grade field is requied just when job is student

--- src/pages/StudentRegister/validation.ts
+++ src/pages/StudentRegister/validation.ts
@@ -1,9 +1,14 @@
 import * as Yup from 'yup';
 
 export const registerValidationSchema = Yup.object().shape({
-  name: Yup.string().required('This field is required'),
-  age: Yup.string().required('This field is required'),
-  job: Yup.string().required('This field is required'),
-  grade: Yup.string().optional(),
-  englishLevel: Yup.string().required('This field is required'),
+  name: Yup.string().required('Name field is required'),
+  age: Yup.string().required('Age field is required'),
+  job: Yup.string().required('Job field is required'),
+  grade: Yup.string()
+    .optional()
+    .when('job', {
+      is: (job: string) => job === 'Student',
+      then: Yup.string().required('Grade field is requird'),
+    }),
+  englishLevel: Yup.string().required('English level field is required'),
 });
