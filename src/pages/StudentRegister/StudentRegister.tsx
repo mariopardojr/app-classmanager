da99060a907c9bcb84f2835111d412401b225ca5
Mario Pardo <mariio.pardojr@gmail.com>
2022-03-04 23:56:17 -0300

    fix: ensure register form validate

--- src/pages/StudentRegister/StudentRegister.tsx
+++ src/pages/StudentRegister/StudentRegister.tsx
@@ -1,14 +1,15 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { Formik, useFormik } from 'formik';
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, ScrollView, GestureResponderEvent, KeyboardAvoidingView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button, Switch, TextInput } from 'react-native-paper';
 import Header from '../../components/Header/Header';
 import fonts from '../../styles/fonts';
-import { registerValidationSchema } from './validation';
 import BackIcon from '../../assets/arrow-left.svg'
 import { StudentRegisterProps } from './types';
 import Input from '../../components/Input/Input';
+import StudentService from '../../services/studentService';
+import { registerValidationSchema as registerSchema } from './validation';
+import { Formik } from 'formik';
 
 const initialValues = {
   name: '',
@@ -19,10 +20,7 @@ const initialValues = {
 }
 
 const StudentRegister: React.FC<StudentRegisterProps> = ({ navigation }) => {
-  const handleNavigateToGoBack = () => navigation.goBack()
-  const { setErrors, values } = useFormik({ initialValues, onSubmit: () => {
-    console.log('values =>', values)
-  }})
+  const handleNavigateToGoBack = () => navigation.navigate('Home')
   const [isStudent, setIsStudent] = useState(true)
 
   const handleSwitch = () => {
@@ -40,100 +38,96 @@ const StudentRegister: React.FC<StudentRegisterProps> = ({ navigation }) => {
                 </TouchableOpacity>
               </View>
               <Header text="New student" />
-              <View style={style.formContainer}>
-                <Formik
-                  initialValues={initialValues}
-                  onSubmit={(values) => console.log(values)}
-                  validationSchema={registerValidationSchema}
-                  enableReinitialize
-                  validateOnBlur={false}
-                  validateOnChange
-                >
-                  {({ handleChange, handleSubmit, handleBlur, values, errors, setFieldValue }) => (
-                    <View>
-                      <Input
-                        style={style.input}
-                        error={!!errors.name}
-                        label="Name"
-                        value={values.name}
-                        onChangeText={handleChange('name')}
-                        onBlur={handleBlur('name')}
-                        right={<TextInput.Icon name="account" color="#5201ba"/>}
-                        helperText={errors.name}
-                        visible={!!errors.name }
-                      />
-                      <Input
-                        style={style.input}
-                        error={!!errors.age}
-                        label="Age"
-                        value={values.age}
-                        onChangeText={handleChange('age')}
-                        onBlur={handleBlur('age')}
-                        right={<TextInput.Icon name="calendar-clock" color="#5201ba"/>}
-                        helperText={errors.age}
-                        visible={!!errors.age}
-                        keyboardType='numeric'
-                      />
-                      <View style={style.jobContainer}>
-                        <Input
-                          style={{ ...style.input, width: 170 }}
-                          error={!!errors.job}
-                          disabled={isStudent}
-                          label="Job"
-                          value={values.job}
-                          onChangeText={handleChange('job')}
-                          onBlur={handleBlur('job')}
-                          right={<TextInput.Icon name="briefcase" color="#5201ba" disabled={isStudent}/>}
-                          helperText={errors.job}
-                          visible={!!errors.job}
-                        />
-                        <View style={style.toogle}>
-                          <Text style={style.toogleText}>Is he/she student?</Text>
-                          <Switch
-                            value={isStudent}
-                            onValueChange={() => {
-                              handleSwitch()
-                              setFieldValue('job', isStudent ? '' : 'Student')
-                            }}
-                            color="#FA743E"
-                          />
-                        </View>
-                      </View>
-                      <Input
-                        style={style.input}
-                        error={!!errors.grade}
-                        disabled={!isStudent}
-                        label="Grade"
-                        value={values.grade}
-                        onChangeText={handleChange('grade')}
-                        onBlur={handleBlur('grade')}
-                        right={<TextInput.Icon name="school" color="#5201ba" disabled={!isStudent}/>}
-                        helperText={errors.grade}
-                        visible={!!errors.grade}
-                      />
-                      <Input
-                        style={style.input}
-                        error={!!errors.englishLevel}
-                        label="English level"
-                        value={values.englishLevel}
-                        onChangeText={handleChange('englishLevel')}
-                        onBlur={handleBlur('englishLevel')}
-                        right={<TextInput.Icon name="head-lightbulb" color="#5201ba"/>}
-                        helperText={errors.englishLevel}
-                        visible={!!errors.englishLevel}
-                      />
-                      <Button
-                        style={{ marginTop: 10 }}
-                        onPress={handleSubmit}
-                        mode='contained'
-                        color="#FA743E"
-                      >
-                        <Text style={style.buttonText}>Submit</Text>
-                      </Button>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={registerSchema}
+                onSubmit={(values) => console.log(values)}
+                validateOnBlur
+              >
+              {({ values, errors, touched, handleBlur, handleChange, handleSubmit, setFieldValue }) => (
+                <View style={style.formContainer}>
+                <Input
+                  style={style.input}
+                  error={!!errors.name && !!touched.name}
+                  label="Name"
+                  value={values.name}
+                  onBlur={handleBlur('name')}
+                  onChangeText={handleChange('name')}
+                  right={<TextInput.Icon name="account" color="#5201ba"/>}
+                  helperText={errors.name}
+                  visible={!!touched.name && !!errors.name}
+                />
+                <Input
+                  style={style.input}
+                  error={!!errors.age && !!touched.age}
+                  label="Age"
+                  value={values.age}
+                  onBlur={handleBlur('age')}
+                  onChangeText={handleChange('age')}
+                  right={<TextInput.Icon name="calendar-clock" color="#5201ba"/>}
+                  helperText={'This field is required'}
+                  visible={!!touched.age && !!errors.age}
+                  keyboardType='numeric'
+                />
+                <View style={style.jobContainer}>
+                  <Input
+                    style={{ ...style.input, width: 170 }}
+                    error={!!errors.job && !!touched.job}
+                    disabled={isStudent}
+                    label="Job"
+                    value={values.job}
+                    onBlur={handleBlur('job')}
+                    onChangeText={handleChange('job')}
+                    right={<TextInput.Icon name="briefcase" color="#5201ba" disabled={isStudent}/>}
+                    helperText={'This field is required'}
+                    visible={!!errors.job && !!touched.job}
+                  />
+                  <View style={style.toogle}>
+                    <Text style={style.toogleText}>Is he/she student?</Text>
+                    <Switch
+                      value={isStudent}
+                      onValueChange={() => {
+                        handleSwitch()
+                        setFieldValue('job', isStudent ? '' : 'Student')
+                      }}
+                      color="#FA743E"
+                    />
                   </View>
-                  )}
-                </Formik>
+                </View>
+                <Input
+                  style={style.input}
+                  error={!!errors.grade && !!touched.grade}
+                  disabled={!isStudent}
+                  label="Grade"
+                  value={values.grade}
+                  onBlur={handleBlur('grade')}
+                  onChangeText={handleChange('grade')}
+                  right={<TextInput.Icon name="school" color="#5201ba" disabled={!isStudent}/>}
+                  helperText={'This field is required'}
+                  visible={!!errors.grade && !!touched.grade}
+                />
+                <Input
+                  style={style.input}
+                  error={!!errors.englishLevel && !!touched.englishLevel}
+                  label="English level"
+                  value={values.englishLevel}
+                  onBlur={handleBlur('englishLevel')}
+                  onChangeText={handleChange('englishLevel')}
+                  right={<TextInput.Icon name="head-lightbulb" color="#5201ba"/>}
+                  helperText={'This field is required'}
+                  visible={!!errors.englishLevel && !!touched.englishLevel}
+                />
+                <Button
+                  style={{ marginTop: 10 }}
+                  onPress={handleSubmit}
+                  mode='contained'
+                  color="#FA743E"
+                >
+                  <Text style={style.buttonText}>Submit</Text>
+                </Button>
               </View>
+              )}
+              </Formik>
             </KeyboardAvoidingView>
           </ScrollView>
         </View>
