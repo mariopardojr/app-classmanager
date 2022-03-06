abafbdc888b15291ac38b090896493a78d8241d4
Mario Pardo <mariio.pardojr@gmail.com>
2022-03-06 04:50:22 -0300

    fix: wrap Note component in a TouchableOpacity

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { style } from './styles';
 import { NoteProps } from './types';
 
 const Note: React.FC<NoteProps> = ({ note }) => {
   return (
-    <View style={style.container}>
-      <Text style={style.title}>{note.title}</Text>
-      <Text style={style.text}>{note.note}</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={style.container}>
+        <Text style={style.title}>{note.title}</Text>
+        <Text style={style.text}>{note.note}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
