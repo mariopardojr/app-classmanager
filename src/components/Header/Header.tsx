4fde306a9edfb641af68f5229cfcfaf0471f4375
Mario Pardo <mariio.pardojr@gmail.com>
2022-03-07 02:06:28 -0300

    style: add animation for header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,24 +3,17 @@ import { Image, Text, View } from 'react-native';
 import { HeaderProps } from './types';
 import AvatarIcon from '../../assets/user-orange.svg';
 import { style } from './styles';
+import * as Animatable from 'react-native-animatable';
 
-const Header: React.FC<HeaderProps> = ({
-  text = 'Hello, ',
-  username,
-  image,
-}) => {
+const Header: React.FC<HeaderProps> = ({ text = 'Hello, ', username, image }) => {
   return (
-    <View style={style.container}>
+    <Animatable.View style={style.container} animation="fadeInRight" iterationCount={1}>
       <View>
         {text && <Text style={style.greeting}>{text}</Text>}
         {username && <Text style={style.username}>{username}</Text>}
       </View>
-      {image ? (
-        <Image source={{ uri: image }} style={style.avatar} />
-      ) : (
-        <AvatarIcon width={150} height={150} />
-      )}
-    </View>
+      {image ? <Image source={{ uri: image }} style={style.avatar} /> : <AvatarIcon width={150} height={150} />}
+    </Animatable.View>
   );
 };
 
